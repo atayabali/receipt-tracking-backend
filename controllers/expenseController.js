b0ed3d807f0217e1771fca78c0dac37a7e0c7139
Adam Tayabali <adamtayabali@gmail.com>
2025-02-10 02:40:37 -0600

    formatting

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -3,15 +3,26 @@ import { mySqlPool } from "../db.js";
 export const getExpenses = async (req, res) => {
   try {
     const data = await mySqlPool.query(
-      "SELECT id, merchant, totalCost, date, hasSubItems FROM expenses"
+      `SELECT id
+        ,merchant
+        ,totalCost
+        ,date
+        ,hasSubItems 
+      FROM expenses 
+      ORDER BY date desc`
     );
-    var summaries = data[0];
-    res.status(200).send(summaries);
+    var expenses = data[0].map((row) => ({
+      ...row,
+      hasSubItems: new Boolean(row.hasSubItems),
+      date: new Date(row.date).toLocaleDateString(), // Or use toLocaleString()
+    }));
+    res.status(200).send(expenses);
   } catch (error) {
     res.status(500).send(error);
   }
 };
 
+//Where did i need this again, maybe for postman testing
 export const getExpenseById = async (req, res) => {
   try {
     var summaryId = req.params.id;
