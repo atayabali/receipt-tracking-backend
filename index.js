756ad4a899a6fdf0844086aa1201be2bb38f328b
Adam Tayabali <adamtayabali@gmail.com>
2025-02-16 23:14:23 -0600

    replace server with index

--- index.js
+++ index.js
@@ -1,134 +1,47 @@
-//AWS EXAMPLE CODE
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import ServerlessHttp from "serverless-http";
+import { router as expenseRouter } from "./routes/expense.js";
+import { router as subExpense } from "./routes/subExpense.js";
+import { router as imageUploadRouter } from "./routes/imageUpload.js";
+dotenv.config();
+//Setup express
+var app = express();
+app.use(cors());
 
-// import { readFile } from "node:fs/promises";
+// Middleware to parse JSON (if needed)
+app.use(express.json()); //Need to understand this better, but req in callback is empty without this
 
-// import {
-//   PutObjectCommand,
-//   S3Client,
-//   S3ServiceException,
-// } from "@aws-sdk/client-s3";
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+//base route won't work in lamdba
+// app.get("/", (req, res) => { 
+//   res.send("Welcome to the server!");
+// });
 
-// /**
-//  * Upload a file to an S3 bucket.
-//  * @param {{ bucketName: string, key: string, filePath: string }}
-//  */
-// export const main = async ({ bucketName, key, filePath }) => {
-//   const client = new S3Client({});
-//   const command = new PutObjectCommand({
-//     Bucket: bucketName,
-//     Key: key,
-//     Body: await readFile(filePath),
-//   });
-
-//   try {
-//     const response = await client.send(command);
-//     console.log(response);
-//   } catch (caught) {
-//     if (
-//       caught instanceof S3ServiceException &&
-//       caught.name === "EntityTooLarge"
-//     ) {
-//       console.error(
-//         `Error from S3 while uploading object to ${bucketName}. \
-// The object was too large. To upload objects larger than 5GB, use the S3 console (160GB max) \
-// or the multipart upload API (5TB max).`,
-//       );
-//     } else if (caught instanceof S3ServiceException) {
-//       console.error(
-//         `Error from S3 while uploading object to ${bucketName}.  ${caught.name}: ${caught.message}`,
-//       );
-//     } else {
-//       throw caught;
-//     }
-//   }
-// };
+app.get("/test", (req, res) => {
+  res.status(200).send("<h1>Nodejs Mysql apps</h1>");
+});
 
 
 
+//routes
+app.use("/uploadImage", imageUploadRouter);
+app.use("/api/v1/expenses", expenseRouter);
+app.use("/api/v1/subexpenses", subExpense);
 
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
 
+const serverless = ServerlessHttp(app);
+export { serverless }
+// Start the server
 
-
-
-
-
-
-
-
-
-
-
-// // // This is used for getting user input.
-// // import { createInterface } from "node:readline/promises";
-
-
-
-
-
-// // export async function main() {
-// //     // A region and credentials can be declared explicitly. For example
-// //   // `new S3Client({ region: 'us-east-1', credentials: {...} })` would
-// //   //initialize the client with those settings. However, the SDK will
-// //   // use your local configuration and credentials if those properties
-// //   // are not defined here.
- 
-
-// //   //I already have a bucket
-// //   // Create an Amazon S3 bucket. The epoch timestamp is appended
-// //   // to the name to make it unique.
-// // //   const bucketName = `test-bucket-${Date.now()}`;
-// // //   await s3Client.send(
-// // //     new CreateBucketCommand({
-// // //       Bucket: bucketName,
-// // //     }),
-// // //   );
-
-
-// //   // Read the object.
-// // //   const { Body } = await s3Client.send(
-// // //     new GetObjectCommand({
-// // //       Bucket: bucketName,
-// // //       Key: "my-first-object.txt",
-// // //     }),
-// // //   );
-
-// // //   console.log(await Body.transformToString());
-
-// //   // Confirm resource deletion.
-// // //   const prompt = createInterface({
-// // //     input: process.stdin,
-// // //     output: process.stdout,
-// // //   });
-
-// // //   const result = await prompt.question("Empty and delete bucket? (y/n) ");
-// // //   prompt.close();
-
-// // //   if (result === "y") {
-// // //     // Create an async iterator over lists of objects in a bucket.
-// // //     const paginator = paginateListObjectsV2(
-// // //       { client: s3Client },
-// // //       { Bucket: bucketName },
-// // //     );
-// // //     for await (const page of paginator) {
-// // //       const objects = page.Contents;
-// // //       if (objects) {
-// // //         // For every object in each page, delete it.
-// // //         for (const object of objects) {
-// // //           await s3Client.send(
-// // //             new DeleteObjectCommand({ Bucket: bucketName, Key: object.Key }),
-// // //           );
-// // //         }
-// // //       }
-// // //     }
-
-// //     // Once all the objects are gone, the bucket can be deleted.
-// //     // await s3Client.send(new DeleteBucketCommand({ Bucket: bucketName }));
-// // //   }
-// // }
-
-// // // Call a function if this file was run directly. This allows the file
-// // // to be runnable without running on import.
-// // import { fileURLToPath } from "node:url";
-// // if (process.argv[1] === fileURLToPath(import.meta.url)) {
-// //   main();
-// // }
